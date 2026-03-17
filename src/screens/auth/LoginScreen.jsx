743c67e57f3d5771d699be85cab224c547012d7d
JanderC <janderalexisc@gmail.com>
2026-03-17 16:54:43 -0400

    cambios

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -92,7 +92,7 @@ const LoginScreen = () => {
                   {/* Logo PAYIFRESH */}
                   <div className="mb-3">
                     <img
-                      src="https://www.pangeatech.com.uy:5334/gestor-documental-dev/EMP-CB4313/servicio/static/original/payifresh-FILE-44C2B9CA.png"
+                      src="https://www.pangeatech.com.uy:5334/gestor-documental-dev/EMP-CB4313/servicio/static/original/payifresh2-FILE-44C2B9CA.png"
                       alt="PAYIFRESH Logo"
                        style={{
                         maxWidth: '280px',
